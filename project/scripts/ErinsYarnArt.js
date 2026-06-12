56c66c3d53b4ee9dbaa4fd61493c5eaf68aeef48
aerohone <aerohone@gmail.com>
2026-06-12 14:49:49 -0600

    project save point, product

---
+++ project/scripts/ErinsYarnArt.js
@@ -0,0 +1,144 @@
+
+const product = [
+    {
+        pn: "1",
+        color: ["pink", "purple", "white"],
+        shape: "square",
+        price: 20,
+        image: "images/pn1.webp"
+    },
+    {
+        pn: "2",
+        color: ["blue", "white", "green"],
+        shape: "square",
+        price: 20,
+        image: "images/pn2.webp"
+    },
+    {
+        pn: "3",
+        color: ["purple", "green", "red"],
+        shape: "square",
+        price: 20,
+        image: "images/pn3.webp"
+    },
+    {
+        pn: "4",
+        color: ["blue", "orange", "pink"],
+        shape: "square",
+        price: 20,
+        image: "images/pn4.webp"
+    },
+    {
+        pn: "5",
+        color: ["red", "orange", "yellow"],
+        shape: "square",
+        price: 20,
+        image: "images/pn5.webp"
+    },
+    {
+        pn: "6",
+        color: ["pink"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn6.webp"
+    },
+    {
+        pn: "7",
+        color: ["purple"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn7.webp"
+    },
+    {
+        pn: "8",
+        color: ["yellow"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn8.webp"
+    },
+    {
+        pn: "9",
+        color: ["orange"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn9.webp"
+    },
+    {
+        pn: "10",
+        color: ["green"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn10.webp"
+    },
+    {
+        pn: "11",
+        color: ["turquoise"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn11.webp"
+    },
+    {
+        pn: "12",
+        color: ["peach"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn12.webp"
+    },
+    {
+        pn: "13",
+        color: ["blue"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn13.webp"
+    },
+    {
+        pn: "14",
+        color: ["white"],
+        shape: "wavy donut",
+        price: 20,
+        image: "images/pn14.webp"
+    },
+    {
+        pn: "15",
+        color: ["purple"],
+        shape: "teardrop",
+        price: 20,
+        image: "images/pn15.webp"
+    },
+    {
+        pn: "16",
+        color: ["green"],
+        shape: "teardrop",
+        price: 20,
+        image: "images/pn16.webp"
+    },
+    {
+        pn: "17",
+        color: ["orange"],
+        shape: "teardrop",
+        price: 20,
+        image: "images/pn17.webp"
+    },
+
+    {
+        pn: "21",
+        color: ["pink"],
+        shape: "circle with bow",
+        price: 20,
+        image: "images/pn21.webp"
+    },
+    {
+        pn: "22",
+        color: ["purple"],
+        shape: "circle with bow",
+        price: 20,
+        image: "images/pn22.webp"
+    },
+    {
+        pn: "24",
+        color: ["red", "white"],
+        shape: "heart",
+        price: 20,
+        image: "images/pn24.webp"
+    }
+]
